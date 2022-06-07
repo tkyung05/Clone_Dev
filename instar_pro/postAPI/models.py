7edb0c3f8b507c41bed29c4e83ad7feacdd9b4d3
tkyung05 <tkyung6761@gmail.com>
2022-06-07 20:30:48 +0900

    postAPI, Post 모델, 기타 초기 셋팅 완료.

---
+++ instar_pro/postAPI/models.py
@@ -0,0 +1,10 @@
+from distutils.command.upload import upload
+from django.db import models
+
+
+class Post(models.Model):
+    photo = models.ImageField(upload_to = 'images/%Y/%m/%d')
+    hash_tag = models.CharField(max_length=30, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # User 필드
